b6841e0281f4178f5723a26800a399b87fb7fd43
Denis112345 <40199504+Denis112345@users.noreply.github.com>
2021-04-10 00:00:25 +0500

    COMMIT
    commit

--- cloud/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Artiсles
-
-# Register your models here.
-
-class ArtiсlesAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"title": ("title",)}
-
-admin.site.register(Artiсles, ArtiсlesAdmin)
